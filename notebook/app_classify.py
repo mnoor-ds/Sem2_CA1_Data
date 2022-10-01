5e1b38bd4c19bf0de7a4e8b0797e6e082e5f589a
mnoor-ds <mnoor-ds@mnoor-ds.com>
2022-10-01 12:04:15 +0000

    app_classify.py

--- notebook/app_classify.py
+++ notebook/app_classify.py
@@ -9,8 +9,12 @@ from dash.dependencies import Input, Output, State
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = 'Learn Arabic'
 
 app.layout = html.Div([
+    html.H2('Start Learning Arabic Today!'),
+    html.P('This app can take multiple images and provide the name of ' + 
+                'the objects in Arabic.'),
     dcc.Upload(
         id='upload-image',
         children=html.Div([
@@ -33,32 +37,38 @@ app.layout = html.Div([
     html.Div(id='output-image-upload'),
 ])
 
-def parse_contents(contents, filename, date):
+
+def parse_contents(contents, filename):
     return html.Div([
         html.H5(filename),
-        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        # placeholder for inference from a model
+        html.H5('Arabic label:'),
+
+
 
         # HTML images accept base64 encoded strings in the same format
         # that is supplied by the upload
         html.Img(src=contents),
         html.Hr(),
-        html.Div('Raw Content'),
-        html.Pre(contents[0:200] + '...', style={
-            'whiteSpace': 'pre-wrap',
-            'wordBreak': 'break-all'
-        })
+        #html.Div('Raw Content'),
+        # html.Pre(contents[0:200] + '...', style={
+        #     'whiteSpace': 'pre-wrap',
+        #     'wordBreak': 'break-all' })
     ])
+
 
 @app.callback(Output('output-image-upload', 'children'),
               Input('upload-image', 'contents'),
               State('upload-image', 'filename'),
-              State('upload-image', 'last_modified'))
-def update_output(list_of_contents, list_of_names, list_of_dates):
+              #State('upload-image', 'last_modified')
+              )
+def update_output(list_of_contents, list_of_names):
     if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
+        children = [parse_contents(c, n) for c, n in zip(
+            list_of_contents, list_of_names)]
         return children
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
